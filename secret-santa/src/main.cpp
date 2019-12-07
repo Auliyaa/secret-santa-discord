0e932cf105b5bb3f8a9985ca38ea6fa09cd1dbc1
Ludwig Fiolka <urushiraa@gmail.com>
2019-12-07 09:30:35 +0100

    [develop] Hide token

--- secret-santa/src/main.cpp
+++ secret-santa/src/main.cpp
@@ -3,9 +3,9 @@
 #include <secret-santa/santa.h>
 #include <secret-santa/santa_client.h>
 
-int main()
+int main(int, char** argv)
 {
-  secret::santa::santa_client clt{"MjcyNzk3NjY5NjAzMTQ3Nzc3.Xeq5DA.Uhmi5lrI7a_iGxOWhBXfxSJJX2s", 2};
+  secret::santa::santa_client clt{argv[1], 2};
   clt.set_master("Auliyaa");
   clt.set_server("You need a montage");
   clt.run();
